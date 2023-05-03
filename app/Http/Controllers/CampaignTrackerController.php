17b783dfac25c5defaf9c67ed04fcc0325242560
Istiak Ahamed <sifatistiak@gmail.com>
2023-05-04 01:45:20 +0600

    controller to dispatch data received from query param to job queue

--- app/Http/Controllers/CampaignTrackerController.php
+++ app/Http/Controllers/CampaignTrackerController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\TrackCampaign;
 use Illuminate\Http\Request;
 
 class CampaignTrackerController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $params = [
+            'date' => $request->query('date'),
+            'country_code' => $request->query('country_code'),
+            'campaign_id' => $request->query('cid'),
+            'creative_id' => $request->query('crid'),
+            'browser_id' => $request->query('bid'),
+            'device_id' => $request->query('did'),
+            'count' => $request->query('count'),
+        ];
+        dispatch(new TrackCampaign($params));
+    }
 }
